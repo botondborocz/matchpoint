1dd2c514321d4ec3eeae78a53012cd86f946f4fa
botondborocz <boroczboti17@gmail.com>
2026-06-10 00:05:36 +0200

    fixed animation

--- iosApp/iosApp/NativeMapScreenView.swift
+++ iosApp/iosApp/NativeMapScreenView.swift
@@ -565,7 +565,7 @@ struct NativeMapScreenView: View {
                         let isSelected = selectedTags.contains(opt)
                         if isSelected {
                             Button(action: {
-                                withAnimation(.spring(response: 0.25)) {
+                                withAnimation(.spring(response: 0.25, dampingFraction: 0.825)) {
                                     selectedTags.remove(opt)
                                 }
                             }) {
